aac0458eb33321ae343c9c56ed3b9256dddf048b
Schuyler Lawrence <schuyler.lawrence@gmail.com>
2019-09-10 13:54:58 -0700

    add User model and routes

--- Backend/seed.js
+++ Backend/seed.js
@@ -1,7 +1,20 @@
-const { db, models } = require('./index')
+const { db, models } = require("./index");
 
-const seed = async() => {
-  await db.sync({force: true})
-}
+const seed = async () => {
+  try {
+    await db.sync({ force: true });
 
-module.exports = seed
+    await models.User.create({
+      firstName: "test",
+      lastName: "test",
+      password: "test",
+      email: "test@test.com"
+    });
+
+    await db.close();
+  } catch (error) {
+    console.log(error);
+  }
+};
+seed();
+module.exports = seed;
