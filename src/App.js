b58c2741b75f79cf21e16d9dd214eb85e22bd6fd
Nica Zulueta <nicacassandrazulueta@gmail.com>
2019-09-11 13:38:59 -0700

    categories, no style yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
+import MeatBar from "./components/MeatBar";
+import MixedBar from "./components/MixedBar";
+import VeggieBar from "./components/VeggieBar";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <NavBar />
+        <Route exact path="/meat" component={MeatBar} />
+        <Route exact path="/mixed" component={MixedBar} />
+        <Route exact path="/veggie" component={VeggieBar} />
         <Route exact path="/" component={Home} />
         <Route path="/:filter" render={({ match }) => <Home match={match} />} />
       </Router>
