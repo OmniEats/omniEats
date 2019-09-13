75f6063148e3cccb4fea17afe83311c38cd3cc0d
Blake Alvernaz <balverna@gmail.com>
2019-09-12 22:16:23 -0700

    Testing Heroku App Variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
-const Dotenv = require('dotenv-webpack')
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ],
-  },
-  plugins: [
-    new Dotenv()
-  ]
+const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack");
+
+module.exports = env => {
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    },
+    plugins: [new Dotenv(), new webpack.DefinePlugin(envKeys)]
+  };
 };
