bc297a7ad2fa3f7cec2c766a23271c975c6cf02c
ElectricZebra <nicholasregoli@gmail.com>
2019-09-13 18:31:02 -0700

    map test working

---
+++ tests/googleMaps.test.js
@@ -0,0 +1,26 @@
+require('@babel/polyfill');
+
+const request = require('supertest');
+
+const app = require('../Backend/server');
+
+describe('test google maps loads from /api/googleMaps', function () {
+  test('post/api/googleMaps should send back data', done => {
+    request(app)
+      .post('/api/google')
+      .send({
+              latitude: 37.7749,
+              longitude: -122.4194
+            })
+      .expect(200)
+      .end((err, res) => {
+        if(err) return done(err);
+        else {
+          console.log(res.body[1])
+          expect(res.body).toBeTruthy();
+          expect(res.body[1].name).toBe('Zuni Café');
+          done();
+        }
+      });
+  });
+})
