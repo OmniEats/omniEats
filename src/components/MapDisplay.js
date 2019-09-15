f5c44ef0750cf73eb2905cc6fe33ea2d953ad90f
BAlvernaz <balverna@gmail.com>
2019-09-15 02:37:16 -0700

    storing User Location in Strore

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import Marker from './Marker';
 import { getAllOmniEats, currentLocation } from '../store';
 
 class MapDisplay extends React.Component {
-    constructor(props){
-      super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.center.lat !== this.props.center.lat || prevProps.center.lat !== this.props.center.lat ) {
-      console.log("hello")
-      this.props.getUserLocation()
-      this.props.allOmniEats()
+    if (
+      prevProps.center.lat !== this.props.center.lat ||
+      prevProps.center.lat !== this.props.center.lat
+    ) {
+      console.log('hello');
+      this.props.getUserLocation();
+      this.props.allOmniEats();
     }
   }
 
   componentDidMount() {
-    this.props.getUserLocation()
-    this.props.allOmniEats()
+    this.props.getUserLocation();
+    this.props.allOmniEats();
   }
 
   render() {
-    const { omniEatsRestaurants, center, zoom } = this.props
+    const { omniEatsRestaurants, center, zoom } = this.props;
     return (
       <div
         style={{
@@ -34,24 +37,30 @@ class MapDisplay extends React.Component {
         }}
       >
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.MAPKEY || 'AIzaSyA50mDPBaEgfNWestAu7oPjFK85h1rhE88'}}
+          bootstrapURLKeys={{
+            key: process.env.MAPKEY || 'AIzaSyA50mDPBaEgfNWestAu7oPjFK85h1rhE88'
+          }}
           defaultCenter={center}
           defaultZoom={zoom}
         >
           {omniEatsRestaurants.map(restaurant => {
-
-           return (
-           <Marker
-              restaurantId={restaurant.id}
-              key={restaurant.id}
-              lat={restaurant.latitude}
-              lng={restaurant.longitude}
-              color="blue"
-              name={restaurant.name}
-              googleId={restaurant.googleId}
-              omniRating={restaurant.omniRating ? restaurant.omniRating.rating : 'No Votes Yet'}
-            />
-          )})}
+            return (
+              <Marker
+                restaurantId={restaurant.id}
+                key={restaurant.id}
+                lat={restaurant.latitude}
+                lng={restaurant.longitude}
+                color="blue"
+                name={restaurant.name}
+                googleId={restaurant.googleId}
+                omniRating={
+                  restaurant.omniRating
+                    ? restaurant.omniRating.rating
+                    : 'No Votes Yet'
+                }
+              />
+            );
+          })}
         </GoogleMapReact>
       </div>
     );
@@ -63,14 +72,17 @@ const stateToProps = ({ omniEatsRestaurants, userLocation }) => {
     omniEatsRestaurants,
     center: userLocation,
     zoom: 15
-  }
-}
+  };
+};
 
 const dispatchToProps = dispatch => {
   return {
     allOmniEats: () => dispatch(getAllOmniEats()),
     getUserLocation: () => dispatch(currentLocation())
-  }
-}
+  };
+};
 
-export default connect(stateToProps, dispatchToProps)(MapDisplay);
+export default connect(
+  stateToProps,
+  dispatchToProps
+)(MapDisplay);
