e6854306dc15c1e7b83a9bdb356259e38006571e
BAlvernaz <balverna@gmail.com>
2019-09-15 16:07:21 -0700

    possible test fix part 2

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/omniEats')
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/omniEats', {logging: false})
 
 module.exports = db
