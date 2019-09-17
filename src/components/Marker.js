2b9fccb8b0a2337b709617b1ad19b2e8e26d789b
BAlvernaz <balverna@gmail.com>
2019-09-16 19:00:27 -0700

    Added Missing (

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -18,7 +18,7 @@ const Marker = ({name, lat, lng, restaurantId, color, omniRating, imgRef}) => {
         <div style={{ fontSize: 16 }}>{name}</div>
         <div>OmniRating:{omniRating}</div>
         <div style={{ cursor: 'pointer' }}><Rating restaurantId={restaurantId} /></div>
-        <img src={"https://maps.googleapis.com/maps/api/place/photo?photoreference=" + imgRef + "&sensor=false&maxheight=180&maxwidth=180&key=" + (process.env.MAPKEY || "AIzaSyA50mDPBaEgfNWestAu7oPjFK85h1rhE88" } />
+        <img src={"https://maps.googleapis.com/maps/api/place/photo?photoreference=" + imgRef + "&sensor=false&maxheight=180&maxwidth=180&key=" + (process.env.MAPKEY || "AIzaSyA50mDPBaEgfNWestAu7oPjFK85h1rhE88") } />
       </div>
     </Popup>
   );
