236112a9776daccb75bbc9f3c41c59c40282d792
BAlvernaz <balverna@gmail.com>
2021-05-09 10:22:23 -0700

    Sequelize Error Fix

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/omniEats', {logging: false})
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/omniEats', {logging: false,  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }})
 
 module.exports = db
